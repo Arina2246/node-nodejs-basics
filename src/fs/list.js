9d41ea0b76324b432844d62a799e7e7797cc122c
Arina2246 <arinanebylova@gmail.com>
2023-06-15 19:16:08 +0200

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const list = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const folderPath = path.resolve(__dirname, 'files');
+
+  const checkFolder = fs.access(folderPath);
+  checkFolder
+    .then(() => {
+      fs.readdir(folderPath).then((filenames) => {
+        for (let filename of filenames) {
+          console.log(filename);
+        }
+      });
+    })
+    .catch(() => {
+      throw new Error('FS operation failed');
+    });
 };
 
 await list();
