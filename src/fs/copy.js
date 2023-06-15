e1439297e4bd6bab48a1eceabd2913d093dad506
Arina2246 <arinanebylova@gmail.com>
2023-06-15 19:12:23 +0200

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,38 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const copy = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const folderPath = path.resolve(__dirname, 'files');
+  const copyFolderPath = path.resolve(__dirname, 'files_copy');
+
+  const folderAcess = fs.promises.access(folderPath);
+  const copyFolderAcess = fs.promises.access(copyFolderPath);
+
+  const errorCheck = Promise.allSettled([folderAcess, copyFolderAcess]).then(
+    (results) => {
+      if (
+        results[0].status !== 'fulfilled' ||
+        results[1].status !== 'rejected'
+      ) {
+        throw new Error('FS operation failed');
+      }
+    }
+  );
+  errorCheck
+    .then(() => fs.promises.mkdir(copyFolderPath))
+    .then(() => {
+      fs.promises.readdir(folderPath).then((filenames) => {
+        for (let filename of filenames) {
+          fs.promises.copyFile(
+            path.resolve(folderPath, filename),
+            path.resolve(copyFolderPath, filename)
+          );
+        }
+      });
+    });
 };
 
 await copy();
