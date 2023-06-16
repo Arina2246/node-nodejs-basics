338107f5efbd18bd1a5f2cc42248a630cb90c293
Arina2246 <arinanebylova@gmail.com>
2023-06-16 09:46:36 +0200

    feat: add zip functions

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import { createUnzip } from 'zlib';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const decompress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.resolve(__dirname, 'files', 'archive.gz');
+  const unzipPath = path.resolve(__dirname, 'files', ' fileToCompress.txt');
+
+  const readStream = fs.createReadStream(filePath);
+  const writeStream = fs.createWriteStream(unzipPath);
+
+  readStream.pipe(createUnzip()).pipe(writeStream);
+
+  const removeFile = () => fs.promises.unlink(filePath);
+  readStream.on('close', removeFile);
 };
 
 await decompress();
