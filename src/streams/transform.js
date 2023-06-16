bbe03dd55c598b09702fd8830d5090e4ee59c164
Arina2246 <arinanebylova@gmail.com>
2023-06-16 09:03:57 +0200

    feat: add stream transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,34 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  const reverseString = (str) => {
+    let resStr = str.toString().split('');
+    if (
+      resStr[resStr.length - 1] === '\n' &&
+      resStr[resStr.length - 2] === '\r'
+    ) {
+      resStr =
+        resStr
+          .slice(0, resStr.length - 2)
+          .reverse()
+          .join('') + '\r\n';
+    } else if (resStr[resStr.length - 1] === '\n') {
+      resStr =
+        resStr
+          .slice(0, resStr.length - 1)
+          .reverse()
+          .join('') + '\n';
+    }
+    return resStr;
+  };
+
+  const reversed = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, reverseString(chunk));
+    },
+  });
+
+  process.stdin.pipe(reversed).pipe(process.stdout);
 };
 
 await transform();
